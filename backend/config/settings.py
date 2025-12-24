23b2c06678c2781f767cca0e383fa89e36a86519
ZaphodSchmidt <Zapschmidt@hotmail.com>
2025-12-24 16:21:14 -0600

    i tink it works 100%

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,7 +80,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',
+    'http://127.0.0.1:8000',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
 
 
 # Database
